b7e2c79dadca743b71d069ec0b98f15902db3940
maksym-sk21 <148781411+maksym-sk21@users.noreply.github.com>
2024-02-23 19:14:16 +0200

    Delete my_projects_tt/image_carousel directory

--- my_projects_tt/image_carousel/toggle_text.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('toggleButton');
-    const overlay = document.querySelector('.overlay');
-
-    toggleButton.addEventListener('click', () => {
-        overlay.classList.toggle('hidden');
-    });
-});
