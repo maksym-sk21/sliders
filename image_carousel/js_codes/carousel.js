66ed156b51cfbf6e094bd08d234dbbcc3c273943
maksym-sk21 <148781411+maksym-sk21@users.noreply.github.com>
2024-03-09 23:08:29 +0200

    Update carousel.js

--- image_carousel/js_codes/carousel.js
+++ image_carousel/js_codes/carousel.js
@@ -1,34 +1,47 @@
-const slider = document.querySelector('.slider');
-const dots = document.querySelectorAll('.dot'); // Добавлено получение точек
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = document.querySelector('.slider');
+    const dots = document.querySelectorAll('.dot');
 
-function activate(e) {
-    const items = document.querySelectorAll('.item');
-    const currentDot = Array.from(dots).findIndex(dot => dot.classList.contains('active'));
+    function activate(e) {
+        const items = document.querySelectorAll('.item');
+        const currentDot = Array.from(dots).findIndex(dot => dot.classList.contains('active'));
 
-    if (e.target.matches('.next')) {
-        slider.append(items[0]);
-        updateDots(currentDot + 1);
+        if (e.target.matches('.next')) {
+            slider.append(items[0]);
+            updateDots(currentDot + 1);
+        }
+
+        if (e.target.matches('.prev')) {
+            slider.prepend(items[items.length - 1]);
+            updateDots(currentDot - 1);
+        }
+
+        updateHash();
     }
 
-    if (e.target.matches('.prev')) {
-        slider.prepend(items[items.length - 1]);
-        updateDots(currentDot - 1);
-    }
-}
+    function updateDots(newIndex) {
+        const totalDots = dots.length;
 
-function updateDots(newIndex) {
-    const totalDots = dots.length;
+        if (newIndex < 0) {
+            newIndex = totalDots - 1;
+        } else if (newIndex >= totalDots) {
+            newIndex = 0;
+        }
 
-    // Обработка кругового переключения
-    if (newIndex < 0) {
-        newIndex = totalDots - 1;
-    } else if (newIndex >= totalDots) {
-        newIndex = 0;
+        dots.forEach((dot, index) => {
+            dot.classList.toggle('active', index === newIndex);
+        });
     }
 
-    dots.forEach((dot, index) => {
-        dot.classList.toggle('active', index === newIndex);
+    function updateHash() {
+        const activeSlideIndex = Array.from(dots).findIndex(dot => dot.classList.contains('active'));
+        window.location.hash = `#${activeSlideIndex}`;
+    }
+
+    window.addEventListener('DOMContentLoaded', function () {
+        const requestedSlideIndex = parseInt(window.location.hash.slice(1));
+        updateDots(requestedSlideIndex);
     });
-}
 
-document.addEventListener('click', activate, false);
+    document.addEventListener('click', activate, false);
+});
