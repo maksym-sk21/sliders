6d14efd9034814e504dff6dba9ad13304ce9d64a
maksym-sk21 <148781411+maksym-sk21@users.noreply.github.com>
2024-03-10 12:07:28 +0200

    Update carousel.js

--- image_carousel/js_codes/carousel.js
+++ image_carousel/js_codes/carousel.js
@@ -1,47 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slider = document.querySelector('.slider');
-    const dots = document.querySelectorAll('.dot');
+const slider = document.querySelector('.slider');
+const dots = document.querySelectorAll('.dot');
 
-    function activate(e) {
-        const items = document.querySelectorAll('.item');
-        const currentDot = Array.from(dots).findIndex(dot => dot.classList.contains('active'));
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    const currentDot = Array.from(dots).findIndex(dot => dot.classList.contains('active'));
 
-        if (e.target.matches('.next')) {
-            slider.append(items[0]);
-            updateDots(currentDot + 1);
-        }
-
-        if (e.target.matches('.prev')) {
-            slider.prepend(items[items.length - 1]);
-            updateDots(currentDot - 1);
-        }
-
-        updateHash();
+    if (e.target.matches('.next')) {
+        slider.append(items[0]);
+        updateDots(currentDot + 1);
     }
 
-    function updateDots(newIndex) {
-        const totalDots = dots.length;
-
-        if (newIndex < 0) {
-            newIndex = totalDots - 1;
-        } else if (newIndex >= totalDots) {
-            newIndex = 0;
-        }
-
-        dots.forEach((dot, index) => {
-            dot.classList.toggle('active', index === newIndex);
-        });
+    if (e.target.matches('.prev')) {
+        slider.prepend(items[items.length - 1]);
+        updateDots(currentDot - 1);
     }
 
-    function updateHash() {
-        const activeSlideIndex = Array.from(dots).findIndex(dot => dot.classList.contains('active'));
-        window.location.hash = `#${activeSlideIndex}`;
+    updateHash();
+}
+
+function updateDots(newIndex) {
+    const totalDots = dots.length;
+
+    if (newIndex < 0) {
+        newIndex = totalDots - 1;
+    } else if (newIndex >= totalDots) {
+        newIndex = 0;
     }
 
-    window.addEventListener('DOMContentLoaded', function () {
-        const requestedSlideIndex = parseInt(window.location.hash.slice(1));
-        updateDots(requestedSlideIndex);
+    dots.forEach((dot, index) => {
+        dot.classList.toggle('active', index === newIndex);
     });
+}
 
-    document.addEventListener('click', activate, false);
+function updateHash() {
+    const activeSlideIndex = Array.from(dots).findIndex(dot => dot.classList.contains('active'));
+    window.location.hash = `#${activeSlideIndex}`;
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    let requestedSlideIndex = parseInt(window.location.hash.slice(1));
+
+    if (isNaN(requestedSlideIndex)) {
+        requestedSlideIndex = 0;
+        window.location.hash = '#0';
+    }
+
+    updateDots(requestedSlideIndex);
 });
+
+document.addEventListener('click', activate, false);
