20eef24edc92ab61147320aa743eeca0c674028c
maksym-sk21 <148781411+maksym-sk21@users.noreply.github.com>
2024-03-11 20:32:07 +0200

    Update carousel.js

--- image_carousel/js_codes/carousel.js
+++ image_carousel/js_codes/carousel.js
@@ -1,5 +1,7 @@
 const slider = document.querySelector('.slider');
 const dots = document.querySelectorAll('.dot');
+let touchStartX = 0;
+let touchEndX = 0;
 
 function activate(e) {
     const items = document.querySelectorAll('.item');
@@ -15,6 +17,38 @@ function activate(e) {
         updateDots(currentDot - 1);
     }
 
+    updateHash();
+}
+
+function handleTouchStart(e) {
+    touchStartX = e.touches[0].clientX;
+}
+
+function handleTouchMove(e) {
+    touchEndX = e.touches[0].clientX;
+}
+
+function handleTouchEnd() {
+    handleSwipe();
+}
+
+function handleSwipe() {
+    const deltaX = touchEndX - touchStartX;
+
+    if (deltaX > 20) {
+        // Свайп вправо, выполнить действие для кнопки "предыдущий"
+        prevButtonAction();
+    } else if (deltaX < -20) {
+        // Свайп влево, выполнить действие для кнопки "следующий"
+        nextButtonAction();
+    }
+}
+
+function nextButtonAction() {
+    const items = document.querySelectorAll('.item');
+    slider.append(items[0]);
+    const currentDot = Array.from(dots).findIndex(dot => dot.classList.contains('active'));
+    updateDots(currentDot + 1);
     updateHash();
 }
 
@@ -49,3 +83,6 @@ window.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('click', activate, false);
+slider.addEventListener('touchstart', handleTouchStart);
+slider.addEventListener('touchmove', handleTouchMove);
+slider.addEventListener('touchend', handleTouchEnd);
